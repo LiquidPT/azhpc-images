03453580520ca3c0ef03881a975fff968ca31ead
Matt Fraser <Matt.Fraser@microsoft.com>
2024-09-25 11:20:16 -0700

    add aocc directory

--- mariner/common/install_amd_libs.sh
+++ mariner/common/install_amd_libs.sh
@@ -10,6 +10,8 @@ AOCC_FOLDER_VERSION=$(echo $AOCC_VERSION | cut -d'.' -f1-2 --output-delimiter='-
 AOCC_DOWNLOAD_URL=https://download.amd.com/developer/eula/aocc/aocc-${AOCC_FOLDER_VERSION}/${AOCC_TARBALL}
 AOCC_FOLDER=$(basename $AOCC_TARBALL .tar)
 
+INSTALL_PREFIX=/opt/amd
+
 $COMMON_DIR/download_and_verify.sh $AOCC_DOWNLOAD_URL $AOCC_SHA256
 tar -xvf ${AOCC_TARBALL}
 
