3c9a4edaf810523ba7d757a17e7b3fce78f6625b
Matt Fraser <Matt.Fraser@microsoft.com>
2024-10-01 20:28:07 -0700

    add gdrcopy back

--- mariner/mariner-2.x/mariner-2.0-hpc/install_nvidiagpudriver.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_nvidiagpudriver.sh
@@ -43,23 +43,23 @@ popd
 ./install_nv_peer_memory.sh
 
 # Install GDRCopy
-# gdrcopy_metadata=$(jq -r '.gdrcopy."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
-# GDRCOPY_VERSION=$(jq -r '.version' <<< $gdrcopy_metadata)
-# GDRCOPY_SHA256=$(jq -r '.sha256' <<< $gdrcopy_metadata)
-# GDRCOPY_DISTRIBUTION=$(jq -r '.distribution' <<< $gdrcopy_metadata)
-# TARBALL="v${GDRCOPY_VERSION}.tar.gz"
-# GDRCOPY_DOWNLOAD_URL=https://github.com/NVIDIA/gdrcopy/archive/refs/tags/${TARBALL}
-# $COMMON_DIR/download_and_verify.sh $GDRCOPY_DOWNLOAD_URL $GDRCOPY_SHA256
-# tar -xvf $TARBALL
+gdrcopy_metadata=$(jq -r '.gdrcopy."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
+GDRCOPY_VERSION=$(jq -r '.version' <<< $gdrcopy_metadata)
+GDRCOPY_SHA256=$(jq -r '.sha256' <<< $gdrcopy_metadata)
+GDRCOPY_DISTRIBUTION=$(jq -r '.distribution' <<< $gdrcopy_metadata)
+TARBALL="v${GDRCOPY_VERSION}.tar.gz"
+GDRCOPY_DOWNLOAD_URL=https://github.com/NVIDIA/gdrcopy/archive/refs/tags/${TARBALL}
+$COMMON_DIR/download_and_verify.sh $GDRCOPY_DOWNLOAD_URL $GDRCOPY_SHA256
+tar -xvf $TARBALL
 
-# pushd gdrcopy-${GDRCOPY_VERSION}/packages/
-# CUDA=/usr/local/cuda ./build-rpm-packages.sh
-# rpm -Uvh gdrcopy-kmod-${GDRCOPY_VERSION}-1dkms.${GDRCOPY_DISTRIBUTION}.noarch.rpm
-# rpm -Uvh gdrcopy-${GDRCOPY_VERSION}-1.${GDRCOPY_DISTRIBUTION}.x86_64.rpm
-# rpm -Uvh gdrcopy-devel-${GDRCOPY_VERSION}-1.${GDRCOPY_DISTRIBUTION}.noarch.rpm
-# sed -i "$ s/$/ gdrcopy*/" /etc/dnf/dnf.conf
-# popd
-# $COMMON_DIR/write_component_version.sh "GDRCOPY" ${GDRCOPY_VERSION}
+pushd gdrcopy-${GDRCOPY_VERSION}/packages/
+CUDA=/usr/local/cuda ./build-rpm-packages.sh
+rpm -Uvh gdrcopy-kmod-${GDRCOPY_VERSION}-1dkms.${GDRCOPY_DISTRIBUTION}.noarch.rpm
+rpm -Uvh gdrcopy-${GDRCOPY_VERSION}-1.${GDRCOPY_DISTRIBUTION}.x86_64.rpm
+rpm -Uvh gdrcopy-devel-${GDRCOPY_VERSION}-1.${GDRCOPY_DISTRIBUTION}.noarch.rpm
+sed -i "$ s/$/ gdrcopy*/" /etc/dnf/dnf.conf
+popd
+$COMMON_DIR/write_component_version.sh "GDRCOPY" ${GDRCOPY_VERSION}
 
 # Install nvidia fabric manager (required for ND96asr_v4)
 ./install_nvidia_fabric_manager.sh
